c5274fd2daf48131283d3b7fdc4ae1b4cce2285d
swxk19 <s.wxk19@gmail.com>
2025-05-28 16:38:28 +0800

    Add resume data

---
+++ src/data/resumeData.js
@@ -0,0 +1,81 @@
+export const resumeData = {
+    name: "Kelvin Seow",
+    email: "s.wxk19@gmail.com",
+    phone: "+65 9296 2763",
+    github: "https://github.com/swxk19",
+    linkedin: "https://www.linkedin.com/in/kelvin-seow-swx/",
+    bio: "Aspiring Software Engineer with a passion for Machine Learning and Full-Stack Development. Eager to leverage strong technical skills and a collaborative mindset to build innovative solutions.",
+    experience: [
+      {
+        title: "Machine Learning Intern",
+        company: "DSO National Laboratories",
+        dates: "January 2024 - June 2024",
+        location: "Singapore",
+        responsibilities: [
+          "Developed a machine learning model for ship classification based on emitted sound, achieving 76% accuracy, aligned with open-source research.",
+          "Developed a dataset management tool to streamline data preprocessing and integration into the Machine Learning pipeline.",
+          "Implemented incremental learning, enabling the model to learn up to 2 new classes.",
+        ],
+      },
+      {
+        title: "Lab Intern",
+        company: "GlaxoSmithKline (GSK)",
+        dates: "September 2018 - February 2019",
+        location: "Singapore",
+        responsibilities: [
+          "Implemented Python scripts to automate inventory management tasks. Improved workflow speed by 70%.",
+          "Maintained tracking of around 200 chemical inventories to ensure accurate stock levels daily.",
+          "Liaised with third-party suppliers to fulfill lab testing demands efficiently.",
+        ],
+      },
+    ],
+    education: [
+      {
+        degree: "Bachelor of Computing in Computer Science",
+        institution: "National University of Singapore (NUS)",
+        year: "Expected 2025",
+        details: ["Second Class Upper"],
+      },
+      {
+        degree: "Diploma with Merit in Applied Chemistry",
+        institution: "Singapore Polytechnic (SP)",
+        year: "2019",
+        details: ["Bronze Medalist"],
+      },
+    ],
+    skills: [
+        { title: "Tools & Frameworks", skillsList: ["MongoDB", "PyTorch", "React", "AWS (EC2)", "PostGresDB", "Docker", "Gradle", "Selenium", "Jest", "Playwright", "JMeter", "FastAPI"] },
+        { title: "Programming Languages", skillsList: ["Python", "SQL", "JavaScript", "TypeScript", "Java", "C"] }
+    ],
+    projects: [
+      {
+        title: "E-commerce Site Testing",
+        dates: "August 2024 - November 2024",
+        description: [
+            "Engineered unit and integration tests for an e-commerce site, achieving code coverage of 95%.",
+            "Created ~75 UI tests to ensure seamless user experience, functionality, and regression prevention."
+        ],
+        techStack: ["MongoDB", "JavaScript", "Jest", "Playwright", "JMeter"],
+        githubLink: "https://github.com/swxk19/cs4218-project-2024-team04/",
+      },
+      {
+        title: "PeerPrep",
+        dates: "August 2023 - November 2023",
+        description: [
+            "Technical interview preparation platform where students can find peers to practice interview/coding questions together.",
+            "Led and coordinated a 4-member Agile development team, managing task assignments and workflow to ensure successful completion of sprint objectives.",
+            "Integrated video and chat functionalities to facilitate seamless communication during mock interviews."
+        ],
+        techStack: ["Python", "FastAPI", "Docker", "TypeScript"],
+        githubLink: "https://github.com/swxk19/ay2324s1-course-assessment-g23",
+        teamInfo: "Led a 4-member Agile team."
+      },
+    ],
+    coursework: [
+        {id: "CS3219", name: "Software Engineering Principles and Patterns"},
+        {id: "CS2102", name: "Database Systems"},
+        {id: "CS4218", name: "Software Testing"},
+        {id: "CS4225", name: "Big Data Systems for Data Science"}
+    ]
+  };
+  
